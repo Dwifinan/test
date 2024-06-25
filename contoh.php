75c8b01af582b44d0591c73676c1dafc3ea6bee5
Dwifinan <restudwifinan@gmail.com>
2024-06-26 02:54:22 +0700

    mengubah file contoh

---
+++ contoh.php
@@ -0,0 +1,49 @@
+<?php
+$koneksi = mysqli_connect('localhost', 'root', '', 'test');
+
+if(isset($_POST['proses'])){
+    $judul = $_POST['judul'];
+    $isi   = $_POST['isi'];
+
+    // Perbaikan: Menggunakan sintaks INSERT INTO dengan tanda kurung kurawal {} untuk nilai kolom
+    $query = "INSERT INTO todo (`judul`, `isi`) VALUES ('$judul', '$isi')";
+
+    // Perbaikan: Memeriksa apakah query berhasil dijalankan
+    if(mysqli_query($koneksi, $query)) {
+        echo "Data Berhasil Ditambahkan";
+    } else {
+        echo "Error: " . mysqli_error($koneksi);
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contoh</title>
+</head>
+<body>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>judul</td>
+                <td>:</td>
+                <td><input type="text" name="judul"></td>
+            </tr>
+            <tr>
+                <td>isi</td>
+                <td>:</td>
+                <td><input type="text" name="isi"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><button type="submit" name="proses">Kirim</button></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
